8b2efc0dd2f4038ab4e1c18433ca481216514389
Runyu Zhang <runyuzhang@berkeley.edu>
2015-03-17 20:31:26 -0700

    hw6 done

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -38,6 +38,27 @@ sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
   # 3) convert the non-adopter with probability p
 
   # return the output
+  
+  output.matrix = matrix(0, ncol = n.doctors + 1, nrow = n.doctors, byrow = FALSE)
+  output.matrix[,1] = 1:n.doctors
+  
+#   browser()
+  for (i in 1:n.days+1){
+    if (i > 2){
+      output.matrix[,i] = output.matrix[,i-1]
+    }
+    doctors = sample(1:n.doctors, 2, replace = F)
+    agreements = which(initial.doctors[doctors] == 1)
+    if (length(agreements) == 1){
+      output.matrix[doctors[[agreements]], i] = 1
+      output.matrix[doctors[which(initial.doctors[doctors] != 1)], i] = sample(c(0,1), p = c(1-p, p), size = 1)
+    }
+    else if (length(agreements) == 2){
+      output.matrix[doctors[[1]], i] = 1
+      output.matrix[doctors[[2]], i] = 1
+    }
+  }
+  return(output.matrix)
 
 }
 
@@ -50,4 +71,33 @@ set.seed(42)
 # on x-axis: days,
 # on y-axis : the number of doctors that have already adopted the drug, on that day
 # Put all 5 lines in one figure (e.g. use first plot() then lines() for the subsequent lines)
+
+n.doctors = 100
+
+initial.doctors = sample(0:1, size = n.doctors, replace = T, p = c(0.9, 0.1))
+n.days = 100
+
+p.s = seq(0,0.8, length.out = 5)
+colours = rainbow(5)
+for (j in 1:length(p.s)){
+  print(j)
+  p = p.s[j]
+  num_agreements = 1:n.days
+  x = sim.doctors(initial.doctors, n.doctors, n.days, p)
+  for (i in 2:n.days + 1){
+    num_agreements[i-1] = length(which(x[,i] == 1))
+  }
+  if(j == 1){
+    
+    plot(1:n.days, num_agreements, pch = 46, type = "l", col = colours[j], main = "# of doctors adopting the drug vs. days", xlab = "Days", ylab = "# of doctors adopting the drug")
+  }
+  else{
+    lines(1:n.days, num_agreements, pch = 22, col = colours[j])
+  }
+}
+
+legend(x=70, y=10, legend=p.s, lwd=2,
+       col=colours, title = "p value")
+
+
 
